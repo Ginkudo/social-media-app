068abc6bda7fc9fc89bc5b065302c48da780cac9
Jimmy Phan <phanjimmy02@gmail.com>
2024-04-19 14:33:56 -0400

    Login page

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
