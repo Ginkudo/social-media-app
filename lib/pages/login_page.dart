90b388be3762452f8eb8f2c26927dc1edb08f19e
Jimmy Phan <phanjimmy02@gmail.com>
2024-04-23 17:53:24 -0400

    login page #2

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,3 +5,15 @@ class LoginPage extends StatefulWidget {
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        //insert login here
+      ],
+    ));
+  }
+}
